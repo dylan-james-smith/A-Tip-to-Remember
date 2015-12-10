8dbb6792b9b5c02edf35de266ab9e0c90de5a76d
Dylan James SMith <dsmith95@mail.ccsf.edu>
2015-12-10 10:33:30 -0800

    Initial Commit

---
+++ A Tip to Remember/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A Tip to Remember
+//
+//  Created by Dylan Smith on 12/10/15.
+//  Copyright © 2015 dylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
