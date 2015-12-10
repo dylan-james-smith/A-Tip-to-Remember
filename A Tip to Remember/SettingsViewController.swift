91444ff2db3af6bf3781268bdfa60a0cc256f6c7
Dylan James SMith <dsmith95@mail.ccsf.edu>
2015-12-10 10:45:17 -0800

    Setup Project and add View controllers (remove default VC) to start "A Tip to Remember"

--- A Tip to Remember/SettingsViewController.swift
+++ A Tip to Remember/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingsViewController.swift
 //  A Tip to Remember
 //
 //  Created by Dylan Smith on 12/10/15.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
